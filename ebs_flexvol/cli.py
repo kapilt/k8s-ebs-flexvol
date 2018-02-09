ee1441df7690802de9aeb721ce660d3ca1f8b094
Kapil Thangavelu <kapilt@gmail.com>
2018-02-09 08:44:31 -0500

    simplify

--- ebs_flexvol/cli.py
+++ ebs_flexvol/cli.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 
 import click
 import json
-import hashlib
 import logging
 import volume
 import sys
@@ -17,12 +16,15 @@ class Result(object):
 
 
 def error(msg):
-    print(json.dumps(msg), file=sys.stderr)
+    result = json.dumps(msg)
+    log.info("err response: %s", result)
+    click.echo(result, err=True)
     sys.exit(1)
 
 
 def output(msg):
-    print(json.dumps(msg), file=sys.stdout)
+    result = json.dumps(msg)
+    log.info("result response: %s", result)
     sys.exit(0)
 
 
@@ -38,77 +40,66 @@ def cli():
 
 @cli.command()
 def init():
-    return click.echo(json.dumps(
-        {'status': Result.Success, 'capabilities': {'attach': True}},
-        indent=2))
+    return output({'status': Result.Success, 'capabilities': {'attach': True}})
 
 
 @cli.command()
 @click.argument('params')
 def getvolumename(params):
     params = json.loads(params)
-    return click.echo(json.dumps({
-        'status': Result.NotSupported}))
-
-    click.echo(json.dumps({
-        'status': Result.Success,
-        'volumeName': hashlib.md5(
-            params.get('kubernetes.io/pvOrVolumeName', '')).hexdigest()},
-                          indent=2))
+    if 'volumeId' not in params:
+        error({
+            'status': Result.Error,
+            'reason': 'missing volume-id in volume spec/request'})
+    output({
+        'status': Result.Success, 'volumeName': params['volumeId']})
 
 
 @cli.command()
 @click.argument('params')
 @click.argument('node')
 def attach(params, node):
-    return click.echo(json.dumps(
-        {'status': Result.Success, 'device': '/dev/xvdf'}, indent=2))
+    return output({'status': Result.Success, 'device': '/dev/xvdf'})
 
 
 @cli.command()
 @click.argument('device_name')
 @click.argument('node')
 def detach(device_name, node):
-    return click.echo(json.dumps(
-        {'status': Result.Success}, indent=2))
+    return output({'status': Result.Success})
 
 
 @cli.command()
 @click.argument('options')
 @click.argument('node')
 def isattached(options, node):
-    return click.echo(json.dumps(
-        {'status': Result.Success, 'attached': True}, indent=2))
+    return output({'status': Result.Success, 'attached': True})
 
 
 @cli.command()
 @click.argument('mount_device')
 @click.argument('options')
 def waitforattach(mount_device, options):
-    return click.echo(json.dumps(
-        {'status': Result.Success}, indent=2))
+    return output({'status': Result.Success})
 
 
 @cli.command()
 @click.argument('mount_device')
 @click.argument('options')
 def waitfordetach(mount_device, options):
-    return click.echo(json.dumps(
-        {'status': Result.Success}, indent=2))
+    return output({'status': Result.Success})
 
 
 @cli.command()
 @click.argument("x")
 @click.argument("y")
 def mount(x, y):
-    return click.echo(json.dumps(
-        {'status': Result.Success}, indent=2))
+    return output({'status': Result.Success})
 
 
 @cli.command()
 @click.argument("x")
 @click.argument("y")
 def unmount(x, y):
-    return click.echo(json.dumps(
-        {'status': Result.Success}, indent=2))
+    return output({'status': Result.Success})
 
